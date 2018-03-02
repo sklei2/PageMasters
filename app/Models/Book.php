4a1293de6e56b9461842df4b33a1bffd7a1c2907
sklei2 <stk1151@rit.edu>
2018-03-02 12:45:28 -0500

    updated the Book model so that we can fill the majority of the columsn with mass fill

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+    	'title',
+    	'author',
+    	'rating',
+    	'price',
+    	'isbn'
+    ];
 }
