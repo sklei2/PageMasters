31a65ee2cef76bcfc0c5fda0cd414ee349a2c9e9
sklei2 <stk1151@rit.edu>
2018-03-02 10:46:54 -0500

    made it so that we inject the AdminRepositoryInterface

--- app/Repositories/AdminRepository/AdminRepositoryInterface.php
+++ app/Repositories/AdminRepository/AdminRepositoryInterface.php
@@ -2,13 +2,12 @@
 
 namespace App\Repositories\AdminRepository;
 
-use App\Repositories\BaseRepository;
 use App\Models\Admin as Admin;
 
-interface AdminRepositoryInterface extends BaseRepository {
+interface AdminRepositoryInterface {
 	public function getAll();
 	public function get($id);
 	public function delete($id);
-	public function create(array $data);
-	public function update($id, array $data);
+	public function create(array $attributes);
+	public function update($id, array $attributes);
 }
