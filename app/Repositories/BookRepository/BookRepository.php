c73d0e37db8d12467ce1ef9b96f1772a0a947a96
sklei2 <stk1151@rit.edu>
2018-03-02 21:43:11 -0500

    fixing a copy and paste error where I left '' instead of changing it to ''

--- app/Repositories/BookRepository/BookRepository.php
+++ app/Repositories/BookRepository/BookRepository.php
@@ -8,8 +8,8 @@ use App\Models\Book as Book;
 
 class BookRepository extends BaseRepository implements BookRepositoryInterface {
 
-	public function __construct(Book $admin) {
-		$this->model = $admin;
+	public function __construct(Book $book) {
+		$this->model = $book;
 	}
 
 	public function getAll() {
