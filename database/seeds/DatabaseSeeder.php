d594ba1904660f156cfc6f3f6c716175bac053ad
sklei2 <stk1151@rit.edu>
2018-03-03 12:09:30 -0500

    seeding for students and the pivot tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call('CourseTableSeeder');
         
         $this->call('InstructorTableSeeder');
+        $this->call('StudentTableSeeder');
     }
 }
