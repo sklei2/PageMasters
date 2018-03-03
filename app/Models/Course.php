ea2c84888a134ee00e04c89ff9225c1749013f10
sklei2 <stk1151@rit.edu>
2018-03-03 00:04:09 -0500

    made it so that courses have a linking table to books

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable = ['name', 'description'];
+
+    public function books() {
+    	return $this->belongsToMany('App\Models\Book', 'course_book', 'course_id', 'book_id');
+    }
 }
