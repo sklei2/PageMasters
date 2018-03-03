c93fea28861e17129a5573333d408ad06e665c78
sklei2 <stk1151@rit.edu>
2018-03-03 14:14:49 -0500

    created a migration for the Reviews Table

---
+++ database/migrations/2018_03_03_190528_create_review_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('student_id');
+            $table->integer('book_id');
+            $table->float('rating', 3, 2); // 3 places, 2 after the .
+            $table->text('textReview');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review');
+    }
+}
