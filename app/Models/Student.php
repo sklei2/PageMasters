57f3254157febe0d672d753b063cea4535ad0789
sklei2 <stk1151@rit.edu>
2018-03-03 11:31:29 -0500

    made it so that student has fillables and has the pivot tables for Books and Courses

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = ['fName', 'lName'];
+
+    public function books() {
+    	return $this->belongsToMany('App\Models\Book', 'student_book', 'student_id', 'book_id');
+    }
+
+    public function courses() {
+    	return $this->belongsToMany('App\Models\Course', 'student_course', 'student_id', 'book_id');
+    }
 }
