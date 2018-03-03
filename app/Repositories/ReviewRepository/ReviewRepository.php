3c342e3111691238cc4b9ea5ec4a9f2cbf941bb1
sklei2 <stk1151@rit.edu>
2018-03-03 16:11:37 -0500

    clean up the base repository and make it so that our review repository is able to grab the reviews based off of an id

--- app/Repositories/ReviewRepository/ReviewRepository.php
+++ app/Repositories/ReviewRepository/ReviewRepository.php
@@ -11,4 +11,12 @@ class ReviewRepository extends BaseRepository implements ReviewRepositoryInterfa
 	public function __construct(Review $review) {
 		$this->model = $review;
 	}
+
+	public function getByBookId($id) {
+		return $this->model->where('book_id', '=', $id)->get();
+	}
+
+	public function getByStudentId($id) {
+		return $this->model->where('student_id', '=', $id)->get();
+	}
 }
--- app/Repositories/ReviewRepository/ReviewRepositoryInterface.php
+++ app/Repositories/ReviewRepository/ReviewRepositoryInterface.php
@@ -5,6 +5,8 @@ namespace App\Repositories\ReviewRepository;
 interface ReviewRepositoryInterface {
 	public function getAll();
 	public function get($id);
+	public function getByBookId($id);
+	public function getByStudentId($id);
 	public function delete($id);
 	public function create(array $attributes);
 	public function update($id, array $attributes);
