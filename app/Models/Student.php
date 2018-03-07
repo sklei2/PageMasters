50e72aadb7d843800e5707c3a721605e4fdff9a7
sklei2 <stk1151@rit.edu>
2018-03-06 23:11:32 -0500

    removed a reference to the cart from the student. If you need the cart, ask the cart

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -19,8 +19,4 @@ class Student extends Model
     public function courses() {
     	return $this->belongsToMany('App\Models\Course', 'student_course', 'student_id', 'course_id');
     }
-
-    public function cart() {
-    	return $this->belongsToMany('App\Models\Cart', 'student_cart', 'student_id', 'cart_id');
-    }
 }
