61aa4ef405f83f3eb9070d05e6ddbda9a976ec10
sklei2 <stk1151@rit.edu>
2018-04-27 19:41:44 -0400

    making it so that we update all the enabled properties on the books with the database

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,8 +6,29 @@ window.onload = function() {
 	}
 };
 
-function disableBook() {
-	alert("DISABLE BOOK");
+function disableBooks() {
+	var form = $('#disableBookForm');
+	if (form && form[0]) {
+		for (var i = 0; i < form[0].length; i++) {
+			var input = form[0][i];
+			if (input.type == 'checkbox') {
+				var id = parseInt(input.id.substring(input.id.length -1));
+				var isEnabled = input.checked ? 1 : 0;
+				var data = { "isEnabled": isEnabled };
+				$.ajax({
+					url: 'api/books/' + id,
+					type: 'PUT',
+					data: data,
+					success: function success(data) {
+						console.log(data);
+					},
+					error: function error(data) {
+						console.log(data);
+					}
+				});
+			}
+		}
+	}
 };
 
 function addBookFormSubmission(event) {
