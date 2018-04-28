5870d7d60a5d9c1938770bb95e7142b508dad1a3
sklei2 <stk1151@rit.edu>
2018-04-28 18:48:29 -0400

    re-adding the submit button for the login form.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,7 +61,14 @@
                                             </label>
                                         </div>
                                     </div>
-                                </div>                                
+                                </div> 
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+                                            Login
+                                        </button>
+                                    </div>
+                                </div>
                             </form>
                         </div>
                     </div>
